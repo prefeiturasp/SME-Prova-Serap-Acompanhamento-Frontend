377abea992a6aeaa4b003f496c8ec866d48966e3
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2022-08-11 20:49:14 -0300

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,7 +6,7 @@ for file in $files
 do
   cp $file /tmp/$file
   rm $file
-  sed -e "s/\${VITE_SME_SERAP_ACOMPANHAMENTO_API}/${VITE_SME_SERAP_ACOMPANHAMENTO_API}/" -e "s/\${VITE_SME_SERAP}/${VITE_SME_SERAP}/" -e "s/\${VITE_CHAVE_API}/${VITE_CHAVE_API}/" /tmp/$file > $file
+  envsubst '${VITE_SME_SERAP_ACOMPANHAMENTO_API},${VITE_SME_SERAP},${VITE_CHAVE_API}' < /tmp/$file > $file
 done
 
 nginx -g 'daemon off;'
