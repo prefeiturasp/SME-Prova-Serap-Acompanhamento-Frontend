8dca62c7c1d6a7ed580cbc1438594b84c01386d8
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-08-12 11:38:22 -0300

    Remover chave api

--- src/services/autenticacao-service.ts
+++ src/services/autenticacao-service.ts
@@ -3,13 +3,12 @@ import api from './api';
 
 const URL_DEFAULT = '/api/v1/autenticacao';
 export const URL_AUTENTICACAO_REVALIDAR = `${URL_DEFAULT}/revalidar`;
-const VITE_CHAVE_API = import.meta.env.VITE_CHAVE_API;
 
 const autenticarValidar = (codigo: string): Promise<AxiosResponse> =>
-  api.post(`${URL_DEFAULT}/validar`, { codigo }, { headers: { 'chave-api': VITE_CHAVE_API } });
+  api.post(`${URL_DEFAULT}/validar`, { codigo });
 
 const autenticarRevalidar = (token: string): Promise<AxiosResponse> =>
-  api.post(URL_AUTENTICACAO_REVALIDAR, { token }, { headers: { 'chave-api': VITE_CHAVE_API } });
+  api.post(URL_AUTENTICACAO_REVALIDAR, { token });
 
 export default {
   autenticarValidar,
