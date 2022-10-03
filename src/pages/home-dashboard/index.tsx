4213479e8a601b9c8c8d38097c3f1738d3cda3bb
Caique Siqueira <latorrecaique@gmail.com>
2022-10-03 12:13:12 -0300

    AB#70991 Inclusão do componente de graficos e ajuste no retorno do componente de totais

--- src/pages/home-dashboard/index.tsx
+++ src/pages/home-dashboard/index.tsx
@@ -3,11 +3,13 @@ import Content from '~/components/content';
 import HeaderHomeDashboard from './header';
 import TabelasResumos from './tabelas';
 import Totalizadores from './totalizadores';
+import Graficos from '~/components/graficos';
 
 const HomeDashboard: React.FC = () => {
   return (
     <Content header={<HeaderHomeDashboard />}>
       <Totalizadores />
+      <Graficos />
       <TabelasResumos />
     </Content>
   );
